bcd86e5f4e9f1639ff984ffce6c56c4ee6349c2f
ductv30501 <ductv30501@gmail.com>
2022-06-02 15:04:44 +0700

    Add project files.

---
+++ Assemblies/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyLibrary;
+using System;
+
+namespace Assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50, b = 25;
+            int result;
+            Console.WriteLine("demo assemblies my library ");
+            result = a.add(b);
+            Console.WriteLine($"{a}+{b}={result}");
+            result = a.sub(b);
+            Console.WriteLine($"{a}-{b}={result}");
+        }
+    }
+}
---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class MyClass
+    {
+        public static int add(this int a, int b) => a + b;
+        public static int sub(this int a, int b) => a - b;
+    }
+}
